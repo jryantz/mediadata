e9ef2721375516f3bf2605e560a81e49e7b2ab21
Jon Yantz <jon@yantz.dev>
2024-09-14 12:02:17 -0400

    Expanded support for actions and sources

--- mediadata/core/management/commands/chapters.py
+++ mediadata/core/management/commands/chapters.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-from mediadata.core.management.base import BaseCommand
-
 from mediadata.contrib.file.actions import (
-    get_chapters_from_silence as get_file_chapters,
+    get_chapters_from_silence,
 )
 from mediadata.contrib.musicbrainz.actions import (
-    get_chapters as get_musicbrainz_chapters,
+    get_chapters as get_chapters_from_musicbrainz,
 )
+from mediadata.contrib.musicbrainz.validators import validate_mbid
+from mediadata.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
@@ -52,38 +52,37 @@ class Command(BaseCommand):
             help="",
         )
 
-    def get_file(self, options: dict):
-        """Get the file and verify that it exists"""
-
-        input_value = options.get("input")
-        input_path = Path(input_value)
-        if not input_path.exists():
-            raise ValueError(f"File not found: {input_value}")
-        absolute_path = input_path.absolute()
-        return absolute_path
-
-    def get_action(self, options: dict):
-        """Get the action and verify that dependencies are met"""
-
-        action = options.get("action")
-        match action:
-            case "generate-file":
-                source = options.get("source")
-                match source:
-                    case "detect-silence":
-                        pass
-                    case "musicbrainz":
-                        musicbrainz_id = options.get("musicbrainz_id")
-
     def handle(self, *args, **options):
-        file = self.get_file(options.get("input"))
-        action = options.get("action")
+        file = _get_file(options)
+        action, kwargs = _get_action(file, options)
+        response = action(**kwargs)
+        print(response)
 
-        # source = str(options["source"])
-        source = options.get("source")
 
-        if source == "musicbrainz":
-            id = str(options["musicbrainz_id"])
-            x = get_file_chapters(file)
-            print(x)
-            # data = get_musicbrainz_data(ref)
+def _get_file(options: dict):
+    """Get the file and verify that it exists"""
+
+    input_value = options["input"][0]
+    input_path = Path(input_value)
+    if not input_path.exists():
+        raise ValueError(f"File not found: {input_value}")
+    absolute_path = input_path.absolute()
+    return absolute_path
+
+
+def _get_action(file: Path, options: dict):
+    """Get the action and verify that dependencies are met"""
+
+    action = options.get("action")
+    match action:
+        case "generate-file":
+            source = options.get("source")
+            match source:
+                case "detect-silence":
+                    return get_chapters_from_silence, {"file": file}
+                case "musicbrainz":
+                    musicbrainz_id = options.get("musicbrainz_id")
+                    validate_mbid(musicbrainz_id)
+                    return get_chapters_from_musicbrainz, {"id": musicbrainz_id}
+
+    raise ValueError(f"Action {action} not supported")
